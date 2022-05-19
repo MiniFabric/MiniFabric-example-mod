4908d8072e8e4fc2dbb2634d0e898fc318927909
PseudoDistant <javafarlander2002@gmail.com>
2022-05-19 10:55:05 -0400

    Should work now

--- src/main/java/io/github/minifabric/example/mixin/ExampleMixin.java
+++ src/main/java/io/github/minifabric/example/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
 package io.github.minifabric.example.mixin;
 
-import com.mojang.ld22.Game;
+import minicraft.core.Game;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -9,8 +9,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Game.class)
 public class ExampleMixin {
 
-	@Inject(at = @At("HEAD"), method = "setMenu")
-	private void init(CallbackInfo info) {
+	@Inject(at = @At("HEAD"), method = "main")
+	private static void init(CallbackInfo info) {
 		System.out.println("This line is printed by an example mod mixin!");
 	}
 }
